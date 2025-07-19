0a4b89110a86516cc0b7232dc055b6344fe7cf8a
Supratim Chakraborty <supratim12111996@gmail.com>
2025-07-19 08:24:48 +0530

    implement custom user admin and custom user creation form

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,21 +1,57 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserChangeForm
 
 from .models import User
+from .forms import CustomUserCreationForm
 
 # Register your models here.
+# class CustomUserAdmin(UserAdmin):
+#     # Making password field non-editable
+#     fieldsets = []
+#     filter_horizontal = []
+#     list_filter = []
+
+#     # changing the display menu of User model at admin panel
+#     list_display = ['email', 'first_name', 'last_name', 'is_active']
+
+#     # ordering the list of users depending on the date_joined field descending
+#     ordering = ['-date_joined']
+    
+
+
+# admin.site.register(User, CustomUserAdmin)
+
+
+
 class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    model = User
+
     # Making password field non-editable
     fieldsets = []
     filter_horizontal = []
     list_filter = []
 
-    # changing the display menu of User model at admin panel
-    list_display = ['email', 'first_name', 'last_name', 'is_active']
 
-    # ordering the list of users depending on the date_joined field descending
-    ordering = ['-date_joined']
-    
+    list_display = ['email', 'is_staff', 'is_active','last_login',]
+    list_filter = ['role',]
+    ordering = ['email',]
+    search_fields = ['email', 'first_name', 'last_name',]
+    readonly_fields = ['date_joined', 'created_at', 'updated_at', 'last_login',]
 
+
+    fieldsets = [
+        [None, {'fields': ['email', 'password', 'first_name', 'last_name', 'username', 'phone_number', 'role']}],
+        ['Permissions', {'fields': ['is_admin', 'is_staff', 'is_active', 'is_superadmin',]}],
+        ['Important dates', {'fields': ['date_joined', 'created_at', 'updated_at', 'last_login',]}],
+    ]
+
+    add_fieldsets = [
+        [None, {
+            'classes': ['wide',],
+            'fields': ['email', 'password', 'confirm_password', 'first_name', 'last_name', 'phone_number', 'role', 'is_active']},
+        ],
+    ]
 
 admin.site.register(User, CustomUserAdmin)
---
+++ accounts/forms.py
@@ -0,0 +1,28 @@
+# yourapp/forms.py
+
+from django import forms
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from .models import User
+
+class CustomUserCreationForm(forms.ModelForm):
+    """Form for creating new users in admin with email instead of username."""
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+    confirm_password = forms.CharField(label='Confirm Password', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['email',]
+
+    def clean_confirm_password(self):
+        password = self.cleaned_data.get("password")
+        confirm_password = self.cleaned_data.get("confirm_password")
+        if password and confirm_password and password != confirm_password:
+            raise forms.ValidationError("Passwords do not match")
+        return confirm_password
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password"])
+        if commit:
+            user.save()
+        return user
