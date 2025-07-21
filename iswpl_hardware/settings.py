136cc494142b5340f009edfd1cda00703465c042
Supratim Chakraborty <supratim12111996@gmail.com>
2025-07-21 08:39:58 +0530

    implement login page and settings for templates and static files

--- iswpl_hardware/settings.py
+++ iswpl_hardware/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'iswpl_hardware.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,6 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR /'static'
+STATICFILES_DIRS = [
+    'iswpl_hardware/static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
