f3cdc42e27b40bb9bfe4ea591ec024444f58ea5c
Supratim Chakraborty <supratim12111996@gmail.com>
2025-07-22 08:01:03 +0530

    implement login check logic

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from django.contrib import auth
 
 # Create your views here.
 def login(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+        
+        user = auth.authenticate(email=email, password=password)
+
+        if user:
+            print("Login")
+        else:
+            print("Invalid")
+
     return render(request, "accounts/login.html")
